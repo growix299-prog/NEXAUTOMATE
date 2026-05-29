b7d5f5ef26490e33eb39fadb2e0be63e12388ab5
boldmonk89 <tejascodes2006@gmail.com>
2026-05-29 23:58:40 +0530

    feat: integrate premium animated emojis into welcome message

--- telegram_bot/handlers/menu.py
+++ telegram_bot/handlers/menu.py
@@ -78,18 +78,18 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     banner = (
             f"HI 🫲😀🫱 {html.escape(user.first_name)}\n"
-            f"WELCOME TO 🎬\n"
+            f"WELCOME TO <tg-emoji emoji-id=\"5289722755871162900\">🎬</tg-emoji>\n"
             f"▬▬▬▬▬▬▬▬▬▬▬\n"
             f"𝐆𝐀𝐌𝐄𝐒 𝐀𝐍𝐃 𝐎𝐓𝐓 𝐁𝐎𝐓\n"
             f"▬▬▬▬▬▬▬▬▬▬▬\n"
-            f"📌 QUICK GUIDE :\n"
-            f"🔖 𝚃𝙰𝙿 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂' 𝙱𝚄𝚃𝚃𝙾𝙽.\n"
-            f"🔖 𝚃𝙰𝙿 '𝙾𝚃𝚃' 𝙾𝚁 '𝙶𝙰𝙼𝙴𝚂' 𝚃𝙾 𝙱𝚁𝙾𝚆𝚂𝙴 𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂.\n"
-            f"🔖 𝙲𝙷𝙾𝙾𝚂𝙴 𝚃𝙷𝙴 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃.\n"
-            f"🔖 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴 𝚃𝙷𝙴 '𝙿𝙰𝚈𝙼𝙴𝙽𝚃'.\n"
-            f"🔖 𝚈𝙾𝚄𝚁 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚆𝙸𝙻𝙻 𝙱𝙴 𝙳𝙴𝙻𝙸𝚅𝙴𝚁𝙴𝙳 𝙸𝙽𝚂𝚃𝙰𝙽𝚃𝙻𝚈.\n"
+            f"<tg-emoji emoji-id=\"5895242866057286055\">📌</tg-emoji> QUICK GUIDE :\n"
+            f"<tg-emoji emoji-id=\"5346105514575025401\">🔖</tg-emoji> 𝚃𝙰𝙿 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂' 𝙱𝚄𝚃𝚃𝙾𝙽.\n"
+            f"<tg-emoji emoji-id=\"5346105514575025401\">🔖</tg-emoji> 𝚃𝙰𝙿 '𝙾𝚃𝚃' 𝙾𝚁 '𝙶𝙰𝙼𝙴𝚂' 𝚃𝙾 𝙱𝚁𝙾𝚆𝚂𝙴 𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂.\n"
+            f"<tg-emoji emoji-id=\"5346105514575025401\">🔖</tg-emoji> 𝙲𝙷𝙾𝙾𝚂𝙴 𝚃𝙷𝙴 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃.\n"
+            f"<tg-emoji emoji-id=\"5346105514575025401\">🔖</tg-emoji> 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴 𝚃𝙷𝙴 '𝙿𝙰𝚈𝙼𝙴𝙽𝚃'.\n"
+            f"<tg-emoji emoji-id=\"5346105514575025401\">🔖</tg-emoji> 𝚈𝙾𝚄𝚁 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚆𝙸𝙻𝙻 𝙱𝙴 𝙳𝙴𝙻𝙸𝚅𝙴𝚁𝙴𝙳 𝙸𝙽𝚂𝚃𝙰𝙽𝚃𝙻𝚈.\n"
             f"▬▬▬▬▬▬▬▬▬▬▬\n"
-            f"📌 PLEASE CHOOSE A MENU BELOW 👇👇👇"
+            f"<tg-emoji emoji-id=\"5895242866057286055\">📌</tg-emoji> PLEASE CHOOSE A MENU BELOW <tg-emoji emoji-id=\"5406745015365943482\">👇</tg-emoji>"
         )
 
     # Send the bottom reply keyboard first
