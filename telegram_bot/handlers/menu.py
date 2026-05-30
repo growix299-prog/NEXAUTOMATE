744ac6e4340422fba1b637059bcd890fdd4c9db6
boldmonk89 <tejascodes2006@gmail.com>
2026-05-30 14:54:32 +0530

    UI text update: Reverted horizontal lines to original length

--- telegram_bot/handlers/menu.py
+++ telegram_bot/handlers/menu.py
@@ -79,16 +79,16 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     banner = (
             f"<b>HI</b> 🫲<tg-emoji emoji-id=\"5456258317477230911\">😎</tg-emoji>🫱 <b>{html.escape(user.first_name)}</b>\n"
             f"<b>WELCOME TO</b> <tg-emoji emoji-id=\"5352625743081775722\">🔘</tg-emoji>\n"
-            f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
+            f"▬▬▬▬▬▬▬▬▬▬▬\n"
             f"𝐆𝐀𝐌𝐄𝐒 𝐀𝐍𝐃 𝐎𝐓𝐓 𝐁𝐎𝐓 <tg-emoji emoji-id=\"5314391089514291948\">🤖</tg-emoji>\n"
-            f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
+            f"▬▬▬▬▬▬▬▬▬▬▬\n"
             f"<tg-emoji emoji-id=\"5222444124698853913\">🔖</tg-emoji> <b><u>QUICK GUIDE</u> :</b>\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝚃𝙰𝙿 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂' 𝙱𝚄𝚃𝚃𝙾𝙽.\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝚃𝙰𝙿 '𝙾𝚃𝚃' 𝙾𝚁 '𝙶𝙰𝙼𝙴𝚂' 𝚃𝙾 𝙱𝚁𝙾𝚆𝚂𝙴 𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂.\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝙲𝙷𝙾𝙾𝚂𝙴 𝚃𝙷𝙴 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃.\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴 𝚃𝙷𝙴 '𝙿𝙰𝚈𝙼𝙴𝙽𝚃'.\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝚈𝙾𝚄𝚁 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚆𝙸𝙻𝙻 𝙱𝙴 𝙳𝙴𝙻𝙸𝚅𝙴𝚁𝙴𝙳 𝙸𝙽𝚂𝚃𝙰𝙽𝚃𝙻𝚈.\n"
-            f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
+            f"▬▬▬▬▬▬▬▬▬▬▬\n"
             f"<b>PLEASE CHOOSE A MENU BELOW</b> <tg-emoji emoji-id=\"5406745015365943482\">⬇️</tg-emoji><tg-emoji emoji-id=\"5406745015365943482\">⬇️</tg-emoji><tg-emoji emoji-id=\"5406745015365943482\">⬇️</tg-emoji>"
         )
 
@@ -218,16 +218,16 @@ async def handle_callback_query(update: Update, context: ContextTypes.DEFAULT_TY
             banner = (
                 f"<b>HI</b> 🫲<tg-emoji emoji-id=\"5456258317477230911\">😎</tg-emoji>🫱 <b>{html.escape(user.first_name)}</b>\n"
                 f"<b>WELCOME TO</b> <tg-emoji emoji-id=\"5352625743081775722\">🔘</tg-emoji>\n"
-                f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
+                f"▬▬▬▬▬▬▬▬▬▬▬\n"
                 f"𝐆𝐀𝐌𝐄𝐒 𝐀𝐍𝐃 𝐎𝐓𝐓 𝐁𝐎𝐓 <tg-emoji emoji-id=\"5314391089514291948\">🤖</tg-emoji>\n"
-                f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
+                f"▬▬▬▬▬▬▬▬▬▬▬\n"
                 f"<tg-emoji emoji-id=\"5222444124698853913\">🔖</tg-emoji> <b><u>QUICK GUIDE</u> :</b>\n"
                 f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝚃𝙰𝙿 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂' 𝙱𝚄𝚃𝚃𝙾𝙽.\n"
                 f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝚃𝙰𝙿 '𝙾𝚃𝚃' 𝙾𝚁 '𝙶𝙰𝙼𝙴𝚂' 𝚃𝙾 𝙱𝚁𝙾𝚆𝚂𝙴 𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂.\n"
                 f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝙲𝙷𝙾𝙾𝚂𝙴 𝚃𝙷𝙴 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃.\n"
                 f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴 𝚃𝙷𝙴 '𝙿𝙰𝚈𝙼𝙴𝙽𝚃'.\n"
                 f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝚈𝙾𝚄𝚁 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚆𝙸𝙻𝙻 𝙱𝙴 𝙳𝙴𝙻𝙸𝚅𝙴𝚁𝙴𝙳 𝙸𝙽𝚂𝚃𝙰𝙽𝚃𝙻𝚈.\n"
-                f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
+                f"▬▬▬▬▬▬▬▬▬▬▬\n"
                 f"<b>PLEASE CHOOSE A MENU BELOW</b> <tg-emoji emoji-id=\"5406745015365943482\">⬇️</tg-emoji><tg-emoji emoji-id=\"5406745015365943482\">⬇️</tg-emoji><tg-emoji emoji-id=\"5406745015365943482\">⬇️</tg-emoji>"
             )
             try:
@@ -284,16 +284,16 @@ async def handle_callback_query(update: Update, context: ContextTypes.DEFAULT_TY
         banner = (
             f"<b>HI</b> 🫲<tg-emoji emoji-id=\"5456258317477230911\">😎</tg-emoji>🫱 <b>{html.escape(user.first_name)}</b>\n"
             f"<b>WELCOME TO</b> <tg-emoji emoji-id=\"5352625743081775722\">🔘</tg-emoji>\n"
-            f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
+            f"▬▬▬▬▬▬▬▬▬▬▬\n"
             f"𝐆𝐀𝐌𝐄𝐒 𝐀𝐍𝐃 𝐎𝐓𝐓 𝐁𝐎𝐓 <tg-emoji emoji-id=\"5314391089514291948\">🤖</tg-emoji>\n"
-            f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
+            f"▬▬▬▬▬▬▬▬▬▬▬\n"
             f"<tg-emoji emoji-id=\"5222444124698853913\">🔖</tg-emoji> <b><u>QUICK GUIDE</u> :</b>\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝚃𝙰𝙿 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂' 𝙱𝚄𝚃𝚃𝙾𝙽.\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝚃𝙰𝙿 '𝙾𝚃𝚃' 𝙾𝚁 '𝙶𝙰𝙼𝙴𝚂' 𝚃𝙾 𝙱𝚁𝙾𝚆𝚂𝙴 𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝚂.\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝙲𝙷𝙾𝙾𝚂𝙴 𝚃𝙷𝙴 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃.\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴 𝚃𝙷𝙴 '𝙿𝙰𝚈𝙼𝙴𝙽𝚃'.\n"
             f"<tg-emoji emoji-id=\"5346105514575025401\">▶️</tg-emoji> 𝚈𝙾𝚄𝚁 '𝙿𝚁𝙾𝙳𝚄𝙲𝚃' 𝚆𝙸𝙻𝙻 𝙱𝙴 𝙳𝙴𝙻𝙸𝚅𝙴𝚁𝙴𝙳 𝙸𝙽𝚂𝚃𝙰𝙽𝚃𝙻𝚈.\n"
-            f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
+            f"▬▬▬▬▬▬▬▬▬▬▬\n"
             f"<b>PLEASE CHOOSE A MENU BELOW</b> <tg-emoji emoji-id=\"5406745015365943482\">⬇️</tg-emoji><tg-emoji emoji-id=\"5406745015365943482\">⬇️</tg-emoji><tg-emoji emoji-id=\"5406745015365943482\">⬇️</tg-emoji>"
         )
         try:
